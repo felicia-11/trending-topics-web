150857de680e67dd24b3ac2253491328690186fb
Felicia <feliciachristiany96@gmail.com>
2024-05-16 15:57:06 +0700

    feat: create header

---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className="relative flex w-full items-center p-4 justify-start gap-2 bg-sky-300 text-white lg:flex-wrap lg:px-8">
+          <Link href="/" className='flex gap-2 items-center'>
+            <Image
+              src="/trending-topics-logo.png"
+              alt="logo"
+              width={30}
+              height={30}
+            />
+          </Link>
+        </header>
+    );
+};
