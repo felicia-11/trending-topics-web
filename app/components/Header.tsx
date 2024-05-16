8d56d96b1b064fbc18523b301f93ddd8ed677229
Felicia <feliciachristiany96@gmail.com>
2024-05-16 16:48:30 +0700

    feat: create parallax title

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="relative flex w-full items-center p-4 justify-start gap-2 bg-sky-300 text-white lg:flex-wrap lg:px-8">
+        <header className="relative flex w-full items-center p-4 justify-start gap-2 bg-gray-300 text-white lg:flex-wrap lg:px-8">
           <Link href="/" className='flex gap-2 items-center'>
             <Image
               src="/trending-topics-logo.png"
